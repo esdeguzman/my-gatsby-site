8ea251a99bdb6fd0c62980aeb2f163666a47a221
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2023-09-25 14:28:17 +0800

    Finished Gatsby Tutorial Part 5

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -8,10 +8,12 @@ const BlogPage = ({ data }) => {
     <Layout pageTitle="My Blog Posts">
       <ul>
       {
-        data.allFile.nodes.map(node => (
-          <li key={node.name}>
-            {node.name}
-          </li>
+        data.allMdx.nodes.map((node) => (
+          <article key={node.id}>
+            <h2>{node.frontmatter.title}</h2>
+            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.excerpt}</p>
+          </article>
         ))
       }
       </ul>
@@ -23,9 +25,14 @@ export const Head = () => <Seo title="My Blog Posts" />
 
 export const query = graphql`
   query {
-    allFile {
+    allMdx(sort: { frontmatter: { date: DESC }}) {
       nodes {
-        name
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+        }
+        id
+        excerpt
       }
     }
   }
